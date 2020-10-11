1a3c03eb3bb5b4108d92c5e315309bf0c754d5a4
Amar Vijaykumar Sigh <56508546+amarvsingh@users.noreply.github.com>
2020-10-11 14:53:40 +0530

    Added roles and question for webd, handled firebase security

--- public/Model/redirect.js
+++ public/Model/redirect.js
@@ -5,25 +5,49 @@ function validateAndRedirect(){
     var code = document.getElementById("code").value;
     var contact = local.concat(code);
     var college = document.getElementById("college").value;
+    var year =  document.getElementById("year").value;
     var role = document.getElementById("role").value;
-    if (fullname != null && email != null && local != null && code != null && college != null && role != null){
-        var url;
-        if (role == "Role 1"){
-            url = "Role1.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&role="+role;
+    if (fullname != null && email != null && code != null && college != null && role != null && year!=null){
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (re.test(String(email).toLowerCase())){
+            if (code.length == 10){
+                var url;
+                if (role == "Technology Specialist"){
+                    url = "Role1.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Web Designer"){
+                    url = "role2.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Graphic Designer"){
+                    url = "role3.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Social Media Marketing Managers"){
+                    url = "role4.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Sponsorship & Experience"){
+                    url = "role5.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Video Production"){
+                    url = "role6.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Event producers and managers"){
+                    url = "role7.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Communication Managers"){
+                    url = "role8.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                else if (role == "Editorial Laison"){
+                    url = "role9.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&year="+year+"&role="+role;
+                }
+                window.location.replace(url);
+            }
+            else{
+                alert("Enter a valid contact information");
+            }
         }
-        else if (role == "Role 2"){
-            url = "role2.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&role="+role;
+        else{
+            alert("Enter a valid email address");
         }
-        else if (role == "Role 3"){
-            url = "role3.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&role="+role;
-        }
-        else if (role == "Role 4"){
-            url = "role4.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&role="+role;
-        }
-        else if (role == "Role 5"){
-            url = "role5.html?fullname="+fullname+"&email="+email+"&phone="+contact+"&college="+college+"&role="+role;
-        }
-        window.location.replace(url);
     }
     else{
         alert("Please fill all the fields");
